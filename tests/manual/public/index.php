1fa832b84b3f4d7fd49f5bc344b64d3e32ccbdf9
Aleksander <mrhdolek14@gmail.com>
2022-02-28 22:10:40 +0100

    fix: add index for test a manual

---
+++ tests/manual/public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use Blumilk\Meetup\Core\MeetupApplication;
+
+require __DIR__ . '../../vendor/autoload.php';
+
+$application = new MeetupApplication((string)__DIR__."/../");
+$application->run();
