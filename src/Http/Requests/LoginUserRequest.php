5d39aac6b635c72147cf1c21c5ef53fda4878bd7
Michał Gałęzyka <72552648+Majkulnn@users.noreply.github.com>
2022-03-03 13:38:52 +0100

    #6 - Apply suggestions from code review
    Co-authored-by: Kamil Piech <kamil.piech.97@gmail.com>

--- src/Http/Requests/LoginUserRequest.php
+++ src/Http/Requests/LoginUserRequest.php
@@ -8,11 +8,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginUserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
