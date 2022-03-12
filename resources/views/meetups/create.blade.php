e110a3286d83e5296b26dc440369a886109a14b2
MichalMyskow <myskow.michal@gmail.com>
2022-03-12 23:19:01 +0100

    #10 - create input error component

--- resources/views/meetups/create.blade.php
+++ resources/views/meetups/create.blade.php
@@ -5,52 +5,32 @@
         <div>
             <h1>Create Meetup</h1>
             @auth
-                <form action="{{ route('meetups') }}" method="post">
+                <form action="{{ route('meetups.store') }}" method="post">
                     @csrf
                     <div>
                         <label for="title">Title:</label>
                         <input type="text" id="title" name="title" value="{{ old('title') }}">
-                        @error('title')
-                        <div>
-                            {{ $message }}
-                        </div>
-                        @enderror
+                        <x-input-error for="title"/>
                     </div>
                     <div>
                         <label for="description">Description:</label>
                         <textarea name="description" id="description" cols="30" rows="4">{{ old('description') }}</textarea>
-                        @error('description')
-                        <div>
-                            {{ $message }}
-                        </div>
-                        @enderror
+                        <x-input-error for="description"/>
                     </div>
                     <div>
                         <label for="date">Date:</label>
                         <input type="dateTime-local" id="date" name="date" value="{{ old('date') }}">
-                        @error('date')
-                        <div>
-                            {{ $message }}
-                        </div>
-                        @enderror
+                        <x-input-error for="date"/>
                     </div>
                     <div>
                         <label for="place">Place:</label>
                         <input type="text" id="place" name="place" value="{{ old('place') }}">
-                        @error('place')
-                        <div>
-                            {{ $message }}
-                        </div>
-                        @enderror
+                        <x-input-error for="place"/>
                     </div>
                     <div>
                         <label for="language">Language:</label>
                         <input type="text" id="language" name="language" value="{{ old('language') }}">
-                        @error('language')
-                        <div>
-                            {{ $message }}
-                        </div>
-                        @enderror
+                        <x-input-error for="language"/>
                     </div>
                     <div>
                         <button type="submit">Post</button>
--- resources/views/organizations/edit.blade.php
+++ resources/views/organizations/edit.blade.php
@@ -11,119 +11,67 @@
                 <div>
                     <label for="name">Name:</label>
                     <input type="text" id="name" name="name" value="{{ old('name', $organization->name) }}">
-                    @error('name')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="name"/>
                 </div>
                 <div>
                     <label for="description">Description:</label>
                     <textarea name="description" id="description" cols="30" rows="4">{{ old('description', $organization->description) }}</textarea>
-                    @error('description')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="description"/>
                 </div>
                 <div>
                     <label for="location">Location:</label>
                     <input type="text" id="location" name="location" value="{{ old('location', $organization->location) }}">
-                    @error('location')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="location"/>
                 </div>
                 <div>
                     <label for="organization_type">Organization type:</label>
                     <input type="text" id="organization_type" name="organization_type" value="{{ old('organization_type', $organization->organization_type) }}">
-                    @error('organization_type')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="organization_type"/>
                 </div>
                 <div>
                     <label for="foundation_date">Foundation date:</label>
                     <input type="dateTime-local" id="foundation_date" name="foundation_date" value="{{ old('foundation_date', $organization->foundation_date) }}">
-                    @error('foundation_date')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="foundation_date"/>
                 </div>
                 <div>
                     <label for="number_of_employers">Number of employers:</label>
                     <input type="text" id="number_of_employers" name="number_of_employers" value="{{ old('number_of_employers', $organization->number_of_employers) }}">
-                    @error('number_of_employers')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="number_of_employers"/>
                 </div>
                 <div>
                     <label for="website_url">Website:</label>
                     <input type="text" id="website_url" name="website_url" value="{{ old('website_url', $organization->website_url) }}">
-                    @error('website_url')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="website_url"/>
                 </div>
                 <div>
                     <label for="facebook_url">Facebook:</label>
                     <input type="text" id="facebook_url" name="facebook_url" value="{{ old('facebook_url', $organization->facebook_url) }}">
-                    @error('facebook_url')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="website_url"/>
                 </div>
                 <div>
                     <label for="linkedin_url">Linkedin:</label>
                     <input type="text" id="linkedin_url" name="linkedin_url" value="{{ old('linkedin_url', $organization->linkedin_url) }}">
-                    @error('linkedin_url')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="linkedin_url"/>
                 </div>
                 <div>
                     <label for="instagram_url">Instagram:</label>
                     <input type="text" id="instagram_url" name="instagram_url" value="{{ old('instagram_url', $organization->instagram_url) }}">
-                    @error('instagram_url')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="instagram_url"/>
                 </div>
                 <div>
                     <label for="youtube_url">YouTube:</label>
                     <input type="text" id="youtube_url" name="youtube_url" value="{{ old('youtube_url', $organization->youtube_url) }}">
-                    @error('youtube_url')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="youtube_url"/>
                 </div>
                 <div>
                     <label for="twitter_url">Twitter:</label>
                     <input type="text" id="twitter_url" name="twitter_url" value="{{ old('twitter_url', $organization->twitter_url) }}">
-                    @error('twitter_url')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="twitter_url"/>
                 </div>
                 <div>
                     <label for="github_url">Github:</label>
                     <input type="text" id="github_url" name="github_url" value="{{ old('github_url', $organization->github_url) }}">
-                    @error('github_url')
-                    <div>
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <x-input-error for="github_url"/>
                 </div>
                 <div>
                     <button type="submit">Update</button>
