5c6070e303c922921487efa0589dc8b90f18dccf
MichalMyskow <myskow.michal@gmail.com>
2022-03-12 23:20:34 +0100

    #10 - change route names

--- src/Http/Routing/WebRouting.php
+++ src/Http/Routing/WebRouting.php
@@ -16,7 +16,7 @@ class WebRouting extends Routing
         $this->router->controller(MeetupController::class)->middleware("auth")->group(function (): void {
             $this->router->get("/meetups", "index")->name("meetups");
             $this->router->get("/meetups/create", "create")->name("meetups.create");
-            $this->router->post("/meetups", "store")->name("meetups");
+            $this->router->post("/meetups", "store")->name("meetups.store");
             $this->router->get("/meetups/{meetup}/edit", "edit")->name("meetups.edit");
             $this->router->put("/meetups/{meetup}", "update")->name("meetups.update");
             $this->router->delete("/meetups/{meetup}", "destroy")->name("meetups.destroy");
@@ -25,7 +25,7 @@ class WebRouting extends Routing
         $this->router->controller(OrganizationController::class)->middleware("auth")->group(function (): void {
             $this->router->get("/organizations", "index")->name("organizations");
             $this->router->get("/organizations/create", "create")->name("organizations.create");
-            $this->router->post("/organizations", "store")->name("organizations");
+            $this->router->post("/organizations", "store")->name("organizations.store");
             $this->router->get("/organizations/{organization}/edit", "edit")->name("organizations.edit");
             $this->router->put("/organizations/{organization}", "update")->name("organizations.update");
             $this->router->delete("/organizations/{organization}", "destroy")->name("organizations.destroy");
