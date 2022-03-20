aa05fd5f0fdcfff4362aaf0675a92499fc7b93f8
Michał Gałęzyka <majkulnspam@gmail.com>
2022-03-20 18:18:55 +0100

    #6 - update services and env

--- config/services.php
+++ config/services.php
@@ -19,11 +19,11 @@ return [
     "google" => [
         "client_id" => env("GOOGLE_CLIENT_ID"),
         "client_secret" => env("GOOGLE_CLIENT_SECRET"),
-        "redirect" => env("GOOGLE_REDIRECT"),
+        "redirect" => env("GOOGLE_REDIRECT", env("APP_URL") . "/auth/google/callback"),
     ],
     "facebook" => [
         "client_id" => env("FACEBOOK_CLIENT_ID"),
         "client_secret" => env("FACEBOOK_CLIENT_SECRET"),
-        "redirect" => env("GOOGLE_REDIRECT"),
+        "redirect" => env("GOOGLE_REDIRECT", env("APP_URL") . "/auth/facebook/callback"),
     ],
 ];
