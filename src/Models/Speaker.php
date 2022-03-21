134c2cb7712190dc9539f2e7a20fac582ca4c02e
Aleksander <mrhdolek14@gmail.com>
2022-03-21 20:29:50 +0100

    fix: after code review

--- src/Models/Speaker.php
+++ src/Models/Speaker.php
@@ -6,7 +6,6 @@ namespace Blumilk\Meetup\Core\Models;
 
 use Blumilk\Meetup\Core\Formats;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Speaker extends Model
