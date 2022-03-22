c9b861a09f621b1f5c1b335e02169f56b09d191b
MichalMyskow <myskow.michal@gmail.com>
2022-03-22 03:16:48 +0100

    #23 - add routing for Contact

--- src/Http/Routing/WebRouting.php
+++ src/Http/Routing/WebRouting.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Blumilk\Meetup\Core\Http\Routing;
 
+use Blumilk\Meetup\Core\Http\Controllers\ContactController;
 use Blumilk\Meetup\Core\Http\Controllers\MeetupController;
 use Blumilk\Meetup\Core\Http\Controllers\OrganizationController;
 
@@ -29,6 +30,11 @@ class WebRouting extends Routing
             $this->router->get("/organizations/{organization}/edit", "edit")->name("organizations.edit");
             $this->router->put("/organizations/{organization}", "update")->name("organizations.update");
             $this->router->delete("/organizations/{organization}", "destroy")->name("organizations.destroy");
+        });
+
+        $this->router->controller(ContactController::class)->group(function (): void {
+            $this->router->get("/contact", "create")->name("contact");
+            $this->router->post("/contact", "store")->name("contact.store");
         });
     }
 }
