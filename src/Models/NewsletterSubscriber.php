5d0bb20abdb7e0c265e94208d01461f5a88b9486
Michał Gałęzyka <majkulnspam@gmail.com>
2022-03-29 15:17:34 +0200

    #22 - create migration and model

---
+++ src/Models/NewsletterSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Blumilk\Meetup\Core\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class NewsletterSubscriber extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "email",
+        "status",
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
