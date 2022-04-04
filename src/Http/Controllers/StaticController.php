86ee5d12d0e623640760de8bd3193147c97734c1
Aleksander <mrhdolek14@gmail.com>
2022-04-04 08:44:07 +0200

    fix: after code review

--- src/Http/Controllers/StaticController.php
+++ src/Http/Controllers/StaticController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Blumilk\Meetup\Core\Http\Controllers;
 
-use Blumilk\Meetup\Core\FilesFilter;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\File;
@@ -12,12 +11,15 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class StaticController extends Controller
 {
+    protected const IGNORE_FILES = [
+        ".gitignore",
+    ];
     private const STATIC_PATH = "static";
 
     public function index(Request $request): BinaryFileResponse
     {
         $path = resource_path() . "/" . self::STATIC_PATH . "/" . $request->route("path");
-        if (!File::exists($path) || FilesFilter::filter(File::basename($path))) {
+        if (!File::exists($path) || $this->filesFilter(File::basename($path))) {
             abort(Response::HTTP_NOT_FOUND);
         }
 
@@ -26,5 +28,10 @@ class StaticController extends Controller
         ];
 
         return response()->file($path, $headers);
+    }
+
+    protected function filesFilter(string $fileName): bool
+    {
+        return in_array($fileName, self::IGNORE_FILES, true);
     }
 }
