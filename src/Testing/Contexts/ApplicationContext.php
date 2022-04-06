79857a1cef0c45fe8d1ff6797ebf87648dc287c7
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2022-04-06 08:37:11 +0200

    #46 - bootstrapper overwritten

--- src/Testing/Contexts/ApplicationContext.php
+++ src/Testing/Contexts/ApplicationContext.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Blumilk\Meetup\Core\Testing\Contexts;
 
 use Behat\Behat\Context\Context;
-use Blumilk\BLT\Features\Traits\Environment;
-use Blumilk\Meetup\Core\Testing\Bootstrapper;
+use Blumilk\Meetup\Core\Testing\Contexts\Traits\Environment;
 use PHPUnit\Framework\Assert;
 
 class ApplicationContext implements Context
@@ -19,20 +18,5 @@ class ApplicationContext implements Context
     public function applicationNameIs(string $name): void
     {
         Assert::assertEquals($name, config("app.name"));
-    }
-
-    /**
-     * @Given application is booted with config:
-     */
-    public function applicationIsBootedWithConfig(array $config): void
-    {
-        $bootstrap = new Bootstrapper();
-        $bootstrap->setConfigOverrides($config);
-
-        if (array_key_exists("app.env", $config)) {
-            $bootstrap->setEnvironmentType($config["app.env"]);
-        }
-
-        $bootstrap->boot();
     }
 }
