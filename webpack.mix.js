0e06522c2a51091a281b5c6c07207d3cdbeaed4d
JakubZakowicz <j.zakowicz99@gmail.com>
2022-04-06 23:31:17 +0200

    #16 - tailwind setup

---
+++ webpack.mix.js
@@ -0,0 +1,11 @@
+let mix = require('laravel-mix');
+
+mix
+  .js('resources/js/app.js', 'resources/static/js')
+  .setPublicPath('resources/static')
+  .postCss('resources/css/app.css', 'resources/static/css', [
+    require('tailwindcss'),
+  ]);
+
+  mix.browserSync('127.0.0.1');
+  mix.disableSuccessNotifications();
