b2bb9e005cb43c2416e693c74825d96aa0a3e3ac
Michał Gałęzyka <72552648+Majkulnn@users.noreply.github.com>
2022-04-11 21:11:25 +0200

    #22 - Apply suggestions from code review
    Co-authored-by: Kamil Piech <kamil.piech.97@gmail.com>

--- database/migrations/2022_03_29_120757_create_newsletter_subscribers_table.php
+++ database/migrations/2022_03_29_120757_create_newsletter_subscribers_table.php
@@ -7,9 +7,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create("newsletter_subscribers", function (Blueprint $table): void {
