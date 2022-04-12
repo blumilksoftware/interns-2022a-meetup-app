5bcd6a61d1b507049213d7cd090582a5655a778f
Michał Gałęzyka <72552648+Majkulnn@users.noreply.github.com>
2022-04-12 23:18:33 +0200

    #49 - Apply suggestions from code review
    Co-authored-by: Kamil Piech <kamil.piech.97@gmail.com>

--- resources/views/user/registered.blade.php
+++ resources/views/user/registered.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div style="text-align: center">
-        <h1>You have been registered<br>Please verified your email address</h1>
+        <h1>You have been registered<br>Please verify your email address</h1>
         <a href="{{ route('login') }}"><button type="button">Login Page</button></a>
     </div>
 @endsection
