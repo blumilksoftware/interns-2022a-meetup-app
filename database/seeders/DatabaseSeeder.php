54faf0d08b6b369715b535e247961de6458946fd
aleksander.kowalski <aleksander.kowalski@anixe.pl>
2022-04-12 14:20:11 +0200

    fix: after review

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            FrontendSeeder::class,
+            DummyDataSeeder::class,
         ]);
     }
 }
