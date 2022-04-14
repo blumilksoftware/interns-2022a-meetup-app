8588b6cb4b23f4947b7c68b625884c38f5ba3c68
JakubZakowicz <j.zakowicz99@gmail.com>
2022-04-14 19:35:30 +0200

    #16 - add responsiveness to image uploading

--- resources/views/meetups/edit.blade.php
+++ resources/views/meetups/edit.blade.php
@@ -14,12 +14,12 @@
                         </h3>
                     </div>
                     <div class="mt-6 flex flex-col gap-7">
-                        <div class="flex items-center">
+                        <div class="sm:flex items-center">
                             <img id="image" src="{{ asset('static/images/no_image.jpeg') }}" alt="meetup"
-                                class="w-[400px] h-[200px]">
+                                class="w-full sm:w-[400px] h-[200px]">
                             <input type="file" accept="image/*" id="image-input" class="hidden">
                             <label for="image-input"
-                                class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer mx-auto">
+                                class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer mx-auto w-full justify-center mt-3 sm:w-auto sm:mt-0">
                                 <!-- Heroicon name: solid/mail -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
                                     stroke="currentColor" stroke-width="2">
