bbd9bd1624683910e462ac9a752b19d9bb42a354
MichalMyskow <myskow.michal@gmail.com>
2022-04-15 03:32:24 +0200

    #59 - create migration and edit model

--- src/Models/Meetup.php
+++ src/Models/Meetup.php
@@ -19,6 +19,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property string $date
  * @property string $place
  * @property string $language
+ * @property string $logo
  * @property-read Organization|null $organization
  * @property-read Speaker|null $speakers
  * @property-read User $user
@@ -35,6 +36,7 @@ class Meetup extends Model
         "date",
         "place",
         "language",
+        "logo",
     ];
     protected $casts = [
         "date:" . Formats::DATETIME,
