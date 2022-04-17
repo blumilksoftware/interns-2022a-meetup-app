134dae9960491f25b58acb7395e3ca8935c58f38
JakubZakowicz <j.zakowicz99@gmail.com>
2022-04-17 20:48:56 +0200

    #60 - add registered page

--- resources/views/user/registered.blade.php
+++ resources/views/user/registered.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="text-align: center">
-        <h1>You have been registered<br>Please go to login Page</h1>
-        <a href="{{ route('login') }}"><button type="button">Login</button></a>
+    <div class="h-screen text-black">
+        <div class="flex justify-center flex-col items-center h-4/5">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-40 w-40 text-green-400" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <p class="text-3xl mt-5">Registration completed successfully</p>
+            <p class="mt-2 text-lg">
+                Please check your registered email for email verification
+            </p>
+            <a href="{{ route('login') }}" class="w-48 mt-10 bg-indigo-600 text-white text-center p-4 rounded-lg">
+                Go to login page
+            </a>
+        </div>
     </div>
 @endsection
