07aa6cdb71823e063bafc92b8f2c11c48a7d3e41
JakubZakowicz <j.zakowicz99@gmail.com>
2022-04-18 18:53:03 +0200

    #60 - change icons from svg to fontawesome

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -27,7 +27,8 @@
                         <form action="{{ route('login') }}" class="space-y-6 mt-12 relative" action="#" method="POST">
                             @csrf
                             @if (!empty($error))
-                                <div class="absolute text-red-500 -top-6 left-1/2 transform -translate-x-1/2">{{ $error }}</div>
+                                <div class="absolute text-red-500 -top-6 left-1/2 transform -translate-x-1/2">
+                                    {{ $error }}</div>
                             @endif
                             <div>
                                 <label for="email" class="block text-sm font-medium text-gray-700">
@@ -90,24 +91,12 @@
                             <div class="mt-6 text-center">
                                 <a href="{{ route('login.google') }}" type="button"
                                     class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2 w-full justify-center md:w-auto">
-                                    <svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab"
-                                        data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 488 512">
-                                        <path fill="currentColor"
-                                            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z">
-                                        </path>
-                                    </svg>
+                                    <span><i class="fa-brands fa-google fa-lg mr-3"></i></span>
                                     Sign in with Google
                                 </a>
                                 <a href="{{ route('login.facebook') }}" type="button"
                                     class="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2 mt-3 w-full justify-center md:w-auto">
-                                    <svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab"
-                                        data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 320 512">
-                                        <path fill="currentColor"
-                                            d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z">
-                                        </path>
-                                    </svg>
+                                    <i class="fa-brands fa-facebook-f fa-lg mr-3"></i>
                                     Sign in with Facebook
                                 </a>
                             </div>
--- resources/views/user/registered.blade.php
+++ resources/views/user/registered.blade.php
@@ -3,15 +3,12 @@
 @section('content')
     <div class="h-screen text-black">
         <div class="flex justify-center flex-col items-center h-4/5">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-40 w-40 text-green-400" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <p class="text-3xl mt-5">Registration completed successfully</p>
+            <i class="fa-regular fa-circle-check text-green-400 fa-8x"></i>
+            <p class="text-3xl mt-8">Registration completed successfully</p>
             <p class="mt-2 text-lg">
                 Please check your registered email for email verification
             </p>
-            <a href="{{ route('login') }}" class="w-48 mt-10 bg-indigo-600 text-white text-center p-4 rounded-lg">
+            <a href="/login" class="w-48 mt-10 bg-indigo-600 text-white text-center p-4 rounded-lg">
                 Go to login page
             </a>
         </div>
