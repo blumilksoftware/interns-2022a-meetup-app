68cd94296992136c45fc4973c2f814ab9111e085
Michał Gałęzyka <majkulnspam@gmail.com>
2022-04-20 20:54:28 +0200

    #49 - changes after review

--- src/Http/Requests/PasswordReset/PasswordResetRequest.php
+++ src/Http/Requests/PasswordReset/PasswordResetRequest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Blumilk\Meetup\Core\Http\Requests;
+namespace Blumilk\Meetup\Core\Http\Requests\PasswordReset;
 
 use Illuminate\Foundation\Http\FormRequest;
 
