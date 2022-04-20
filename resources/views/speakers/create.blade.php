5134c3539d2461abb66b95390cf925943995db62
JakubZakowicz <j.zakowicz99@gmail.com>
2022-04-20 21:27:02 +0200

    #20 - add create speaker

--- resources/views/speakers/create.blade.php
+++ resources/views/speakers/create.blade.php
@@ -1,42 +1,92 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <div>
-            <h1>Add Speakers</h1>
-            @auth
-                <form action="{{ route('speakers.store') }}" method="post" enctype="multipart/form-data">
-                    @csrf
+    <div class="container md:w-[800px] mx-auto">
+        @auth
+            <form action="{{ route('speakers.store') }}" method="post" enctype="multipart/form-data"
+                class="bg-white p-6 mt-20 rounded-20 shadow-xl">
+                @csrf
+                <div>
                     <div>
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" name="name" value="{{ old('name') }}">
-                        <x-input-error for="name"/>
+                        <h3 class="text-xl leading-6 font-medium text-gray-900">
+                            Create Meetup
+                        </h3>
                     </div>
-                    <div>
-                        <label for="description">Description:</label>
-                        <textarea name="description" id="description" cols="30" rows="4">{{ old('description') }}</textarea>
-                        <x-input-error for="description"/>
+                    <div class="mt-6 flex flex-col gap-7">
+                        <div>
+                            <label for="name" class="block font-medium text-gray-700">
+                                Name
+                            </label>
+                            <div class="mt-1">
+                                <input type="text" name="name" id="name" placeholder="Name..." value="{{ old('name') }}"
+                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                                <x-input-error for="name" />
+                            </div>
+                        </div>
+                        <div>
+                            <label for="description" class="block font-medium text-gray-700">
+                                Description
+                            </label>
+                            <div class="mt-1">
+                                <textarea rows="5" type="text" name="description" id="description" placeholder="Description..."
+                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ old('description') }}</textarea>
+                                <x-input-error for="description" />
+                            </div>
+                        </div>
+                        <div>
+                            <label for="avatar" class="block font-medium text-gray-700">
+                                Avatar
+                            </label>
+                            <div x-data class="flex items-center gap-12 mt-4">
+                                <img x-ref="image" id="image" src="{{ asset('/static/images/no_avatar.png') }}"
+                                    alt="meetup_avatar" class="w-full sm:w-[100px] h-[100px] rounded-full object-cover">
+                                <input @change="image.src = URL.createObjectURL($event.target.files[0])" type="file"
+                                    accept="image/*" id="avatar" name="avatar" class="hidden">
+                                <label for="avatar"
+                                    class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer w-full justify-center mt-3 sm:w-auto sm:mt-0">
+                                    <i class="fa-solid fa-arrow-up-from-bracket mr-3"></i>
+                                    Upload Image
+                                </label>
+                                <x-input-error for="avatar" />
+                            </div>
+                        </div>
+                        <div>
+                            <label for="linkedin_url" class="block font-medium text-gray-700">
+                                Linkedin url
+                            </label>
+                            <div class="mt-1">
+                                <input type="text" id="linkedin_url" name="linkedin_url" value="{{ old('linkedin_url') }}"
+                                    placeholder="Linkedin url..."
+                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                                <x-input-error for="linkedin_url" />
+                            </div>
+                        </div>
+                        <div>
+                            <label for="github_url" class="block font-medium text-gray-700">
+                                Github url
+                            </label>
+                            <div class="mt-1">
+                                <input type="text" id="github_url" name="github_url" value="{{ old('github_url') }}"
+                                    placeholder="Github url..."
+                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                                <x-input-error for="github_url" />
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <label for="avatar">Avatar:</label>
-                        <input type="file" id="avatar" name="avatar">
-                        <x-input-error for="avatar"/>
+                </div>
+                <div class="pt-6">
+                    <div class="flex justify-end">
+                        <a href="{{ route('speakers') }}"
+                            class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Cancel
+                        </a>
+                        <button type="submit"
+                            class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Save
+                        </button>
                     </div>
-                    <div>
-                        <label for="linkedin_url">Linkedin url:</label>
-                        <input type="text" id="linkedin_url" name="linkedin_url" value="{{ old('linkedin_url') }}">
-                        <x-input-error for="linkedin_url"/>
-                    </div>
-                    <div>
-                        <label for="github_url">Github url:</label>
-                        <input type="text" id="github_url" name="github_url" value="{{ old('github_url') }}">
-                        <x-input-error for="github_url"/>
-                    </div>
-                    <div>
-                        <button type="submit">Post</button>
-                    </div>
-                </form>
-            @endauth
-        </div>
+                </div>
+            </form>
+        @endauth
     </div>
 @endsection
