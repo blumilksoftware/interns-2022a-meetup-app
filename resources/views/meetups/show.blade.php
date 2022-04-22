10923ac22ce8a6a4f78ff836c176d59454fe2cbe
MichalMyskow <myskow.michal@gmail.com>
2022-04-22 13:30:03 +0200

    #65 - create basic views

--- resources/views/meetups/show.blade.php
+++ resources/views/meetups/show.blade.php
@@ -20,5 +20,14 @@
         <div>
             {{ $meetup->language }}
         </div>
+        <h1>Speakers:</h1>
+        <div>
+            @foreach ($meetup->speakers as $speaker)
+                <li> {{ $speaker->name }} </li>
+            @endforeach
+        </div>
+        <div>
+            {{ $test }}
+        </div>
     </div>
 @endsection
