c9f97969a36a49102f7f44bed0cc966479d0c195
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-04-25 01:23:07 +0200

    #18 - refactor FormInput.php

--- src/View/Components/FormInput.php
+++ src/View/Components/FormInput.php
@@ -5,32 +5,14 @@ declare(strict_types=1);
 namespace Blumilk\Meetup\Core\View\Components;
 
 use Illuminate\View\Component;
+use Illuminate\View\View;
 
 class FormInput extends Component
 {
-    public $id;
-    public $name;
-    public $type;
-    public $value;
 
-    /**
-     * Create a new component instance.
-     */
-    public function __construct($id, $name, $type, $value)
-    {
-        //
-        $this->id = $id;
-        $this->name = $name;
-        $this->type = $type;
-        $this->value = $value;
-    }
+    public function __construct(public $id, public $name, public $type, public $value) {} 
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
-     */
-    public function render()
+    public function render(): View
     {
         return view("components.form-input");
     }
