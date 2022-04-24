b88388911edd0b2203ac111ea41a749c6af242fa
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-04-24 21:06:31 +0200

    #71 - add newsletter view

--- resources/views/newsletter/index.blade.php
+++ resources/views/newsletter/index.blade.php
@@ -1,34 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <h1>Newsletter Page</h1>
-        @if(session()->has('message'))
-            <div>
-                {{session()->get('message')}}
+  <div class="py-16 sm:py-24">
+    <div class="relative sm:py-16">
+      <div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+        <div class="relative rounded-2xl px-6 py-10 bg-indigo-600 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+          <div aria-hidden="true" class="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
+            <svg class="absolute inset-0 h-full w-full" preserveAspectRatio="xMidYMid slice"
+              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 1463 360">
+              <path class="text-indigo-500 text-opacity-40" fill="currentColor"
+                d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z" />
+              <path class="text-indigo-700 text-opacity-40" fill="currentColor"
+                d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z" />
+            </svg>
+          </div>
+          <div class="relative">
+            <div class="sm:text-center">
+              <h2 class="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">Subscribe to our newsletter</h2>
+              <p class="mt-6 mx-auto max-w-2xl text-lg text-indigo-200">And start getting information about meetups and news</p>
             </div>
-        @endif
-        <form id="newsletterForm" action="" method="post">
-            @csrf
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" value="{{ old('email') }}">
-                <x-input-error for="email"/>
-            </div>
-            <input type="submit" name="Subscribe" value="Subscribe" onclick="subscribe()">
-            <input type="submit" name="Unsubscribe" value="Unsubscribe" onclick="unsubscribe()">
-        </form>
-        <script>
-            const form=document.getElementById("newsletterForm");
-            function subscribe() {
-                form.action="{{ route('newsletter.store') }}";
-                form.submit();
-            }
-            function unsubscribe() {
-                form.action="{{ route('newsletter.destroy') }}";
-                form.submit();
-            }
-
-        </script>
+            <form class="mt-12 sm:mx-auto sm:max-w-lg" method="post">
+              <div class="min-w-0 flex-1">
+                <label for="cta-email" class="sr-only">Email address</label>
+                <input id="cta-email" type="email"
+                  class="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
+                  placeholder="Enter your email">
+              </div>
+              <div class="mt-5 flex justify-between">
+                <button
+                  class="block w-[45%] rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10">Subscribe</button>
+                <button
+                  class="block w-[45%] rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10">Unsubscribe</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 @endsection
