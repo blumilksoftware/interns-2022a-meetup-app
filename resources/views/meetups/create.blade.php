ceab66b762ac41d4d9cc12eb4d20dba2718e768d
MichalMyskow <myskow.michal@gmail.com>
2022-04-29 02:47:36 +0200

    #83 - reformat code

--- resources/views/meetups/create.blade.php
+++ resources/views/meetups/create.blade.php
@@ -4,7 +4,7 @@
     <div class="container md:w-[800px] mx-auto">
         @auth
             <form action="{{ route('meetups.store') }}" method="post" enctype="multipart/form-data"
-                class="bg-white p-6 mt-20 rounded-20 shadow-xl">
+                  class="bg-white p-6 mt-20 rounded-20 shadow-xl">
                 @csrf
                 <div>
                     <div>
@@ -15,11 +15,11 @@
                     <div class="mt-6 flex flex-col gap-7">
                         <div x-data class="sm:flex items-center">
                             <img x-ref="image" id="image" src="{{ asset('/static/images/no_image.webp') }}"
-                                alt="meetup_avatar" class="w-full sm:w-[400px] h-[200px]">
+                                 alt="meetup_avatar" class="w-full sm:w-[400px] h-[200px]">
                             <input @change="image.src = URL.createObjectURL($event.target.files[0])" type="file"
-                                accept="image/*" id="logo" name="logo" class="hidden">
+                                   accept="image/*" id="logo" name="logo" class="hidden">
                             <label for="logo"
-                                class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer mx-auto w-full justify-center mt-3 sm:w-auto sm:mt-0">
+                                   class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer mx-auto w-full justify-center mt-3 sm:w-auto sm:mt-0">
                                 <i class="fa-solid fa-arrow-up-from-bracket mr-3"></i>
                                 Upload Image
                             </label>
@@ -29,9 +29,10 @@
                                 Title
                             </label>
                             <div class="mt-1">
-                                <input type="text" name="title" id="title" placeholder="Title..." value="{{ old('title') }}"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-                                <x-input-error for="title" />
+                                <input type="text" name="title" id="title" placeholder="Title..."
+                                       value="{{ old('title') }}"
+                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+                                <x-input-error for="title"/>
                             </div>
                         </div>
 
@@ -40,9 +41,10 @@
                                 Description
                             </label>
                             <div class="mt-1">
-                                <textarea rows="5" type="text" name="description" id="description" placeholder="Description..."
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ old('description') }}</textarea>
-                                <x-input-error for="description" />
+                                <textarea rows="5" type="text" name="description" id="description"
+                                          placeholder="Description..."
+                                          class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ old('description') }}</textarea>
+                                <x-input-error for="description"/>
                             </div>
                         </div>
 
@@ -52,9 +54,9 @@
                             </label>
                             <div class="mt-1">
                                 <input type="datetime-local" name="date" id="date" placeholder="Date..."
-                                    value="{{ old('date') }}"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-                                <x-input-error for="date" />
+                                       value="{{ old('date') }}"
+                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+                                <x-input-error for="date"/>
                             </div>
                         </div>
 
@@ -63,9 +65,10 @@
                                 Place
                             </label>
                             <div class="mt-1">
-                                <input type="text" name="place" id="place" placeholder="Place..." value="{{ old('place') }}"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-                                <x-input-error for="place" />
+                                <input type="text" name="place" id="place" placeholder="Place..."
+                                       value="{{ old('place') }}"
+                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+                                <x-input-error for="place"/>
                             </div>
                         </div>
 
@@ -75,9 +78,9 @@
                             </label>
                             <div class="mt-1">
                                 <input type="text" name="language" id="language" placeholder="Language..."
-                                    value="{{ old('language') }}"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-                                <x-input-error for="language" />
+                                       value="{{ old('language') }}"
+                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+                                <x-input-error for="language"/>
                             </div>
                         </div>
                         <div>
@@ -104,7 +107,8 @@
                             </label>
                             <div class="mt-1">
                                 <select name="speakers[]" id="speakers"
-                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" multiple="">
+                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                                        multiple>
                                     @foreach ($speakers as $speaker)
                                         <option value="{{ $speaker->id }}" {{ (collect(old('speakers'))->contains($speaker->id)) ? 'selected':'' }}>
                                             {{ $speaker->name }}
@@ -118,11 +122,11 @@
                 <div class="pt-6">
                     <div class="flex justify-end">
                         <a href="{{ route('meetups') }}"
-                            class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                           class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Cancel
                         </a>
                         <button type="submit"
-                            class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Save
                         </button>
                     </div>
--- resources/views/meetups/edit.blade.php
+++ resources/views/meetups/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="container md:w-[800px] mx-auto">
         @auth
             <form action="{{ route('meetups.update', $meetup) }}" method="post"
-                class="bg-white p-6 mt-20 rounded-20 shadow-xl" enctype="multipart/form-data">
+                  class="bg-white p-6 mt-20 rounded-20 shadow-xl" enctype="multipart/form-data">
                 @method('PUT')
                 @csrf
                 <div>
@@ -16,11 +16,11 @@
                     <div class="mt-6 flex flex-col gap-7">
                         <div x-data class="sm:flex items-center">
                             <img x-ref="image" id="image" src="{{ $meetup->logoPath }}" alt="{{ $meetup->title }} logo"
-                                class="w-full sm:w-[400px] h-[200px]">
+                                 class="w-full sm:w-[400px] h-[200px]">
                             <input @change="image.src = URL.createObjectURL($event.target.files[0])" type="file"
-                                accept="image/*" id="logo" class="hidden" name="logo">
+                                   accept="image/*" id="logo" class="hidden" name="logo">
                             <label for="logo"
-                                class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer mx-auto w-full justify-center mt-3 sm:w-auto sm:mt-0">
+                                   class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer mx-auto w-full justify-center mt-3 sm:w-auto sm:mt-0">
                                 <i class="fa-solid fa-arrow-up-from-bracket mr-3"></i>
                                 Upload Image
                             </label>
@@ -31,9 +31,9 @@
                             </label>
                             <div class="mt-1">
                                 <input type="text" name="title" id="title" placeholder="Title..."
-                                    value="{{ old('title', $meetup->title) }}"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-                                <x-input-error for="title" />
+                                       value="{{ old('title', $meetup->title) }}"
+                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+                                <x-input-error for="title"/>
                             </div>
                         </div>
                         <div>
@@ -41,9 +41,10 @@
                                 Description
                             </label>
                             <div class="mt-1">
-                                <textarea rows="5" type="text" name="description" id="description" placeholder="Description..."
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ old('description', $meetup->description) }}</textarea>
-                                <x-input-error for="description" />
+                                <textarea rows="5" type="text" name="description" id="description"
+                                          placeholder="Description..."
+                                          class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ old('description', $meetup->description) }}</textarea>
+                                <x-input-error for="description"/>
                             </div>
                         </div>
                         <div>
@@ -52,9 +53,9 @@
                             </label>
                             <div class="mt-1">
                                 <input type="datetime-local" name="date" id="date" placeholder="Date..."
-                                    value="{{ old('date', $meetup->date) }}"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-                                <x-input-error for="date" />
+                                       value="{{ old('date', $meetup->date) }}"
+                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+                                <x-input-error for="date"/>
                             </div>
                         </div>
                         <div>
@@ -63,9 +64,9 @@
                             </label>
                             <div class="mt-1">
                                 <input type="text" name="place" id="place" placeholder="Place..."
-                                    value="{{ old('place', $meetup->place) }}"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-                                <x-input-error for="place" />
+                                       value="{{ old('place', $meetup->place) }}"
+                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+                                <x-input-error for="place"/>
                             </div>
                         </div>
                         <div>
@@ -74,9 +75,9 @@
                             </label>
                             <div class="mt-1">
                                 <input type="text" name="language" id="language" placeholder="Language..."
-                                    value="{{ old('language', $meetup->language) }}"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-                                <x-input-error for="language" />
+                                       value="{{ old('language', $meetup->language) }}"
+                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
+                                <x-input-error for="language"/>
                             </div>
                         </div>
                         <div>
@@ -103,7 +104,8 @@
                             </label>
                             <div class="mt-1">
                                 <select name="speakers[]" id="speakers"
-                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" multiple="">
+                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                                        multiple>
                                     @foreach ($speakers as $speaker)
                                         <option value="{{ $speaker->id }}"
                                                 {{ (collect(old('speakers'))->contains($speaker->id)) ? 'selected':'' }}
@@ -119,11 +121,11 @@
                 <div class="pt-6">
                     <div class="flex justify-end">
                         <a href="{{ route('meetups') }}"
-                            class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                           class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Cancel
                         </a>
                         <button type="submit"
-                            class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Update
                         </button>
                     </div>
