44e46a81e7f92bff23e34a18583fd768bacb2066
Aleksander <mrhdolek14@gmail.com>
2022-05-01 23:14:58 +0200

    feat: eslint config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,24 @@
 let mix = require('laravel-mix');
 
 mix
-  .js('resources/js/app.js', 'resources/static/js')
-  .setPublicPath('resources/static')
-  .postCss('resources/css/app.css', 'resources/static/css', [
-    require('tailwindcss'),
-  ]);
+    .js('resources/js/app.js', 'resources/static/js')
+    .setPublicPath('resources/static')
+    .postCss('resources/css/app.css', 'resources/static/css', [
+        require('tailwindcss'),
+    ]);
 
-  mix.browserSync('127.0.0.1');
-  mix.disableSuccessNotifications();
+mix.browserSync('127.0.0.1');
+mix.disableSuccessNotifications();
+
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                test: /\.(js)?$/
+            },
+        ]
+    }
+})
