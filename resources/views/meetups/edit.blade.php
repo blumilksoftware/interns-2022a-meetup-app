c831ec0369008958df4c7fe44199ebda0355ea54
MichalMyskow <myskow.michal@gmail.com>
2022-05-03 22:22:38 +0200

    #83 - update views with null input

--- resources/views/meetups/edit.blade.php
+++ resources/views/meetups/edit.blade.php
@@ -66,6 +66,7 @@
                 Speakers
               </label>
               <div class="mt-1">
+                <input type="hidden" name="speakers" value="" />
                 <select name="speakers[]" id="speakers"
                         class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
                         multiple>
