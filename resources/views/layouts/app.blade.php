42b76d0d1930e6a9887cbcd54d25a01d92b035ff
Aleksander Kowalski <mrhdolek14@gmail.com>
2022-05-05 19:14:26 +0200

    #85 -  fix bug for show content a other browsers (#97)
    * fix: bug for show other browsers
    
    * fix: after review
    
    * fix: after review
    
    * fix: after review
    
    Co-authored-by: aleksander.kowalski <aleksander.kowalski@anixe.pl>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('/static/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('/static/css/app.css') }}" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
