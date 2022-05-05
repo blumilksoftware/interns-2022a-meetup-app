3a43853b1130f735c18d57e49454a176457ffdfa
aleksander.kowalski <aleksander.kowalski@anixe.pl>
2022-05-05 21:54:35 +0200

    fix: after review

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,9 @@
 let mix = require('laravel-mix');
-let eslint = require('eslint-webpack-plugin');
 
 mix
     .js('resources/js/app.js', 'resources/static/js')
+    .version()
     .setPublicPath('resources/static')
     .postCss('resources/css/app.css', 'resources/static/css', [
         require('tailwindcss'),
     ]);
-
-mix.browserSync('127.0.0.1');
-mix.disableSuccessNotifications();
-
-module.exports = {
-    plugins: [new eslint(options)],
-};
