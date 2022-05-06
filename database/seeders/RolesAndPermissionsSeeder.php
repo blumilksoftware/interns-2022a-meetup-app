add597e4949bec06c8b0e3bfed0f1907a885b4cb
MichalMyskow <myskow.michal@gmail.com>
2022-05-07 00:04:22 +0200

    #92 - remove permissions from seeder

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
 
@@ -15,30 +14,6 @@ class RolesAndPermissionsSeeder extends Seeder
     {
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        $permissions = [
-            "create meetups",
-            "edit meetups",
-            "delete meetups",
-            "create organizations",
-            "edit organizations",
-            "delete organizations",
-            "create organizationProfiles",
-            "edit organizationProfiles",
-            "delete organizationProfiles",
-            "create speakers",
-            "edit speakers",
-            "delete speakers",
-            "create news",
-            "edit news",
-            "delete news",
-            "delete users",
-        ];
-
-        foreach ($permissions as $permission) {
-            Permission::create(["name" => $permission]);
-        }
-
-        $role = Role::create(["name" => "admin"]);
-        $role->givePermissionTo(Permission::all());
+        Role::create(["name" => "admin"]);
     }
 }
