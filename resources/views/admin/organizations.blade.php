b83c1ec2f5c215996e7086a69acc41c747e60df6
MichalMyskow <myskow.michal@gmail.com>
2022-05-06 20:42:20 +0200

    #65 - delete @if from views

--- resources/views/admin/organizations.blade.php
+++ resources/views/admin/organizations.blade.php
@@ -5,35 +5,32 @@
         <h1>Organizations</h1>
         <div>
             <a href="{{ route('organizations.create') }}">Create new organization</a>
-            @if ($organizations->count())
-                @forelse ($organizations as $organization)
+            @forelse ($organizations as $organization)
+                <div>
+                    Id: {{ $organization->id }}
+                    Name: {{ $organization->name }}
+                    Location: {{ $organization->location }}
+                    Organization type: {{ $organization->organizationType }}
+                    Foundation date: {{ $organization->foundationDate }}
+                    Number of employees: {{ $organization->numberOfEmployees }}
+                    Created at: {{ $organization->createdAt }}
+                    Updated at: {{ $organization->updatedAt }}
                     <div>
-                        Id: {{ $organization->id }}
-                        Name: {{ $organization->name }}
-                        Location: {{ $organization->location }}
-                        Organization type: {{ $organization->organizationType }}
-                        Foundation date: {{ $organization->foundationDate }}
-                        Number of employees: {{ $organization->numberOfEmployees }}
-                        Created at: {{ $organization->createdAt }}
-                        Updated at: {{ $organization->updatedAt }}
-                        <div>
-                            <a href="{{ route('organizations.edit', $organization) }}">Edit</a>
-                            <form action="{{ route('organizations.destroy', $organization) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit"
-                                        onclick="return confirm('Delete organization? This operation is irreversible.')">
-                                    Delete
-                                </button>
-                            </form>
-                        </div>
+                        <a href="{{ route('organizations.edit', $organization) }}">Edit</a>
+                        <form action="{{ route('organizations.destroy', $organization) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                    onclick="return confirm('Delete organization? This operation is irreversible.')">
+                                Delete
+                            </button>
+                        </form>
                     </div>
-                @empty
-                        <p>There are no organizations</p>
-                @endforelse
-
-                {{ $organizations->links('vendor.pagination.tailwind') }}
-            @endif
+                </div>
+            @empty
+                <p>There are no organizations</p>
+            @endforelse
+            {{ $organizations->links('vendor.pagination.tailwind') }}
         </div>
     </div>
 @endsection
--- resources/views/admin/speakers.blade.php
+++ resources/views/admin/speakers.blade.php
@@ -5,34 +5,31 @@
         <h1>Speakers</h1>
         <div>
             <a href="{{ route('speakers.create') }}">Create new speaker</a>
-            @if ($speakers->count())
-                @forelse ($speakers as $speaker)
+            @forelse ($speakers as $speaker)
+                <div>
+                    Id: {{ $speaker->id }}
+                    Name: {{ $speaker->name }}
+                    Description: {{ $speaker->description }}
+                    Linkedin: {{ $speaker->linkedinUrl }}
+                    GitHub: {{ $speaker->githubUrl }}
+                    Created at: {{ $speaker->createdAt }}
+                    Updated at: {{ $speaker->updatedAt }}
                     <div>
-                        Id: {{ $speaker->id }}
-                        Name: {{ $speaker->name }}
-                        Description: {{ $speaker->description }}
-                        Linkedin: {{ $speaker->linkedinUrl }}
-                        GitHub: {{ $speaker->githubUrl }}
-                        Created at: {{ $speaker->createdAt }}
-                        Updated at: {{ $speaker->updatedAt }}
-                        <div>
-                            <a href="{{ route('organizations.edit', $speaker) }}">Edit</a>
-                            <form action="{{ route('organizations.destroy', $speaker) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit"
-                                        onclick="return confirm('Delete speaker? This operation is irreversible.')">
-                                    Delete
-                                </button>
-                            </form>
-                        </div>
+                        <a href="{{ route('organizations.edit', $speaker) }}">Edit</a>
+                        <form action="{{ route('organizations.destroy', $speaker) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                    onclick="return confirm('Delete speaker? This operation is irreversible.')">
+                                Delete
+                            </button>
+                        </form>
                     </div>
-                @empty
-                    <p>There are no speakers</p>
-                @endforelse
-
-                {{ $speakers->links('vendor.pagination.tailwind') }}
-            @endif
+                </div>
+            @empty
+                <p>There are no speakers</p>
+            @endforelse
+            {{ $speakers->links('vendor.pagination.tailwind') }}
         </div>
     </div>
 @endsection
