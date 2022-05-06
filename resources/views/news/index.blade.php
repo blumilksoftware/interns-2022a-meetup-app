7139b66de661710218635b392433499e64ffd703
MichalMyskow <myskow.michal@gmail.com>
2022-05-06 14:47:48 +0200

    #65 - changes in views

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -8,22 +8,25 @@
                 <a href="{{ route('news.create') }}">Create new news</a>
             @endauth
             @if ($news->count())
-                @foreach ($news as $singleNews)
-                    <div>
-                        {{ $singleNews->title }}
-                        {!! Str::markdown($singleNews->text) !!}
-                        {{ $singleNews->author }}
+                @forelse ($news as $singleNews)
+                    <a href="{{ route('meetups.show', $meetup) }}" class="w-[397px] h-[309px] rounded-2xl bg-white shadow-lg">
+                        <div>
+                            {{ $singleNews->title }}
+                            {!! Str::markdown($singleNews->text) !!}
+                            {{ $singleNews->author }}
 
-                        <a href="{{ route('news.edit', $singleNews) }}">Edit</a>
-                        <form action="{{ route('news.destroy', $singleNews) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit"  onclick="return confirm('Delete News? This operation is irreversible.')">Delete</button>
-                        </form>
-                    </div>
-                @endforeach
-            @else
-                <p>There are no news</p>
+                            <a href="{{ route('news.edit', $singleNews) }}">Edit</a>
+                            <form action="{{ route('news.destroy', $singleNews) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" onclick="return confirm('Delete News? This operation is irreversible.')">Delete
+                                </button>
+                            </form>
+                        </div>
+                    </a>
+                @empty
+                    <p>There are no news</p>
+                @endforelse
             @endif
         </div>
     </div>
