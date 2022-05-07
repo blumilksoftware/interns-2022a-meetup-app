c61a4d61ed31de833a2ac946b8fc11680218e3a6
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-05-07 18:34:59 +0200

    #27 - add active navbar items

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -5,7 +5,7 @@
     <h2 class="text-3xl font-bold">News</h2>
     <div class="md:flex mt-3 text-white">
       <a href="#" class="w-1/2 relative">
-        <img src={{ asset('static/images/news_list.webp') }} alt="news" class="w-full h-[450px] object-cover" />
+        <img src="{{ asset('static/images/news_list.webp') }}" alt="news" class="w-full h-[450px] object-cover" />
         <div class="absolute w-[500px] md:w-auto top-64 md:top-52 lg:bottom-0 lg:top-auto z-10 p-10">
           <h3 class="text-2xl font-bold">
             OpenAI is Making Coding As Easy As Talking to a Smart Speaker
@@ -20,7 +20,7 @@
       <div class="w-1/2">
         <a href="#" class="w-1/2 relative">
           <div class="relative">
-            <img src={{ asset('static/images/news_list.webp') }} alt="news" class="w-full h-[225px] object-cover" />
+            <img src="{{ asset('static/images/news_list.webp') }}" alt="news" class="w-full h-[225px] object-cover" />
             <div class="absolute bottom-0 z-10 px-7 py-3">
               <h3 class="text-xl font-bold leading-6">
                 OpenAI is Making Coding As Easy As Talking to a Smart Speaker
@@ -35,7 +35,7 @@
         </a>
         <div class="flex">
           <a href="#" class="relative w-full">
-            <img src={{ asset('static/images/news_list.webp') }} alt="news" class="w-full h-[225px] object-cover" />
+            <img src="{{ asset('static/images/news_list.webp') }}" alt="news" class="w-full h-[225px] object-cover" />
             <div class="absolute bottom-0 z-10 px-5 py-3">
               <h3 class="text-lg font-bold leading-5">
                 OpenAI is Making Coding As Easy As Talking to a Smart
@@ -49,7 +49,7 @@
             <div class="absolute w-full h-full inset-0 bg-gradient-to-t from-white to-black opacity-20"></div>
           </a>
           <a href="#" class="relative w-full">
-            <img src={{ asset('static/images/news_list.webp') }} alt="news" class="w-full h-[225px] object-cover" />
+            <img src="{{ asset('static/images/news_list.webp') }}" alt="news" class="w-full h-[225px] object-cover" />
             <div class="absolute bottom-0 z-10 px-5 py-3">
               <h3 class="text-lg font-bold leading-5">
                 OpenAI is Making Coding As Easy As Talking to a Smart
@@ -60,6 +60,7 @@
                 investigations, and bipartisanship before our eyes
               </p>
             </div>
+            <div class="absolute w-full h-full inset-0 bg-gradient-to-t from-white to-black opacity-20"></div>
           </a>
         </div>
       </div>
@@ -70,7 +71,7 @@
           <li class="mt-7">
             <a href="#">
               <div class="bg-white flex flex-wrap md:flex-nowrap items-center gap-9 shadow-lg rounded-20">
-                <img src={{ asset('static/images/news_list.webp') }} alt="news_image"
+                <img src="{{ asset('static/images/news_list.webp') }}" alt="news_image"
                   class="w-full rounded-t-20 md:w-[361px] md:h-[218px] md:rounded-l-20 md:rounded-tr-none object-cover" />
                 <div class="w-[500px] px-8 pb-5 md:pb-0">
                   <h3 class="text-xl font-bold">
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -18,24 +18,25 @@
         </a>
         <div class="hidden lg:block sm:ml-6">
           <div class="flex space-x-4">
-            <a href="{{ route('home') }}" class="bg-indigo-600 text-white px-3 py-2 rounded-md text-sm font-medium"
+            <a href="{{ route('home') }}"
+              class="text-white {{ request()->routeIs('home') ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 px-3 py-2 rounded-md text-sm font-medium"
               aria-current="page">
               Home
             </a>
             <a href="{{ route('organizations') }}"
-              class="text-white hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              class="text-white {{ request()->routeIs('organizations') ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
               Organizations
             </a>
             <a href="{{ route('speakers') }}"
-              class="text-white hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              class="text-white {{ request()->routeIs('speakers') ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
               Speakers
             </a>
             <a href="{{ route('news') }}"
-              class="text-white hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              class="text-white {{ request()->routeIs('news') ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
               News
             </a>
             <a href="{{ route('newsletter') }}"
-              class="text-white hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              class="text-white {{ request()->routeIs('newsletter') ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
               Newsletter
             </a>
             <a href="#"
@@ -43,7 +44,7 @@
               About
             </a>
             <a href="{{ route('contact') }}"
-              class="text-white hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              class="text-white {{ request()->routeIs('contact') ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
               Contact
             </a>
           </div>
@@ -91,24 +92,24 @@
   </div>
   <div x-cloak x-show="navOpened" x-transition class="lg:hidden" id="mobile-menu">
     <div class="px-2 pt-2 pb-3 space-y-1">
-      <a href="{{ route('home') }}" class="bg-indigo-600 text-white block px-3 py-2 rounded-md text-base font-medium"
+      <a href="{{ route('home') }}" class="text-white {{ (request()->routeIs('home')) ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 block px-3 py-2 rounded-md text-base font-medium"
         aria-current="page">
         Home
       </a>
       <a href="{{ route('organizations') }}"
-        class="text-white hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+        class="text-white {{ (request()->routeIs('organizations')) ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
         Organizations
       </a>
       <a href="{{ route('speakers') }}"
-        class="text-white hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+        class="text-white {{ (request()->routeIs('speakers')) ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
         Speakers
       </a>
       <a href="{{ route('news') }}"
-        class="text-white hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+        class="text-white {{ (request()->routeIs('news')) ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
         News
       </a>
       <a href="{{ route('newsletter') }}"
-        class="text-white hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+        class="text-white {{ (request()->routeIs('newsletter')) ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
         News
       </a>
       <a href="#"
@@ -116,7 +117,7 @@
         About
       </a>
       <a href="{{ route('contact') }}"
-        class="text-white hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+        class="text-white {{ (request()->routeIs('contact')) ? 'bg-indigo-600' : '' }} hover:bg-indigo-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
         Contact
       </a>
     </div>
