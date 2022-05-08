637270cd6bfcad48144f7601f989a62fdd4b229c
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-05-08 14:59:14 +0200

    #27 - fix news overflow

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -5,7 +5,8 @@
     <h2 class="text-3xl font-bold">News</h2>
     <div class="md:flex mt-3 text-white">
       <a href="#" class="w-1/2 relative">
-        <img src="{{ asset('static/images/news_list.webp') }}" alt="news" class="w-full h-[450px] object-cover rounded-t-20 md:rounded-l-20 md:rounded-tr-none" />
+        <img src="{{ asset('static/images/news_list.webp') }}" alt="news"
+          class="w-full h-[450px] object-cover rounded-t-20 md:rounded-l-20 md:rounded-tr-none" />
         <div class="absolute w-[500px] md:w-auto top-64 md:top-52 lg:bottom-0 lg:top-auto z-10 p-10">
           <h3 class="text-2xl font-bold">
             OpenAI is Making Coding As Easy As Talking to a Smart Speaker
@@ -15,12 +16,15 @@
             investigations, and bipartisanship before our eyes
           </p>
         </div>
-        <div class="absolute w-full h-full inset-0 bg-gradient-to-t from-white to-black opacity-20 rounded-t-20 md:rounded-l-20 md:rounded-tr-none"></div>
+        <div
+          class="absolute w-full h-full inset-0 bg-gradient-to-t from-white to-black opacity-20 rounded-t-20 md:rounded-l-20 md:rounded-tr-none">
+        </div>
       </a>
       <div class="md:w-1/2">
         <a href="#" class="w-1/2 relative">
           <div class="relative">
-            <img src="{{ asset('static/images/news_list.webp') }}" alt="news" class="w-full h-[225px] object-cover md:rounded-tr-20" />
+            <img src="{{ asset('static/images/news_list.webp') }}" alt="news"
+              class="w-full h-[225px] object-cover md:rounded-tr-20" />
             <div class="absolute bottom-0 z-10 px-7 py-3">
               <h3 class="text-xl font-bold leading-6">
                 OpenAI is Making Coding As Easy As Talking to a Smart Speaker
@@ -30,12 +34,14 @@
                 investigations, and bipartisanship before our eyes
               </p>
             </div>
-            <div class="absolute w-full h-full inset-0 bg-gradient-to-t from-white to-black opacity-20 md:rounded-tr-20"></div>
+            <div class="absolute w-full h-full inset-0 bg-gradient-to-t from-white to-black opacity-20 md:rounded-tr-20">
+            </div>
           </div>
         </a>
         <div class="flex">
           <a href="#" class="relative w-full">
-            <img src="{{ asset('static/images/news_list.webp') }}" alt="news" class="w-full h-[225px] object-cover rounded-bl-20 md:rounded-bl-none" />
+            <img src="{{ asset('static/images/news_list.webp') }}" alt="news"
+              class="w-full h-[225px] object-cover rounded-bl-20 md:rounded-bl-none" />
             <div class="absolute bottom-0 z-10 px-5 py-3">
               <h3 class="text-lg font-bold leading-5">
                 OpenAI is Making Coding As Easy As Talking to a Smart
@@ -49,7 +55,8 @@
             <div class="absolute w-full h-full inset-0 bg-gradient-to-t from-white to-black opacity-20"></div>
           </a>
           <a href="#" class="relative w-full">
-            <img src="{{ asset('static/images/news_list.webp') }}" alt="news" class="w-full h-[225px] object-cover rounded-br-20" />
+            <img src="{{ asset('static/images/news_list.webp') }}" alt="news"
+              class="w-full h-[225px] object-cover rounded-br-20" />
             <div class="absolute bottom-0 z-10 px-5 py-3">
               <h3 class="text-lg font-bold leading-5">
                 OpenAI is Making Coding As Easy As Talking to a Smart
@@ -77,8 +84,10 @@
                   <h3 class="text-xl font-bold md:mt-10">
                     {{ $singleNews->title }}
                   </h3>
-                    <p class="text-gray-400 mt-5 break-all truncate">
-                    {!! Str::markdown($singleNews->text) !!}
+                  <div class="h-[110px] overflow-hidden">
+                    <p class="text-gray-400 mt-5 break-all">
+                      {!! Str::markdown($singleNews->text) !!}
+                  </div>
                   </p>
                 </div>
               </div>
