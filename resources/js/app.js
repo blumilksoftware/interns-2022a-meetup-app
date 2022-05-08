e16547e3f95fd5ebf8b38fb3eb0826185b1af01c
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-05-08 17:31:38 +0200

    #27 - fix alpine js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,35 @@
-import Alpine from 'alpinejs'
-import Editor from '@toast-ui/editor'
-import 'codemirror/lib/codemirror.css'
-import '@toast-ui/editor/dist/toastui-editor.css'
+import Alpine from 'alpinejs';
+import Editor from '@toast-ui/editor';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
-const editor = new Editor({
-  el: document.querySelector('#editor'),
-  height: '400px',
-  initialEditType: 'markdown',
-  placeholder: 'Write something cool!',
-})
+if (document.querySelector('#editor')) {
+  const editor = new Editor({
+    el: document.querySelector('#editor'),
+    height: '400px',
+    initialEditType: 'markdown',
+    placeholder: 'Write something cool!',
+  });
 
-if (document.querySelector('#createNews')) {
-  document.querySelector('#createNews').addEventListener('submit', event => {
-    event.preventDefault()
-    document.querySelector('#text').value = editor.getMarkdown()
-    event.target.submit()
-  })
+  if (document.querySelector('#createNews')) {
+    document.querySelector('#createNews').addEventListener('submit', event => {
+      event.preventDefault();
+      document.querySelector('#text').value = editor.getMarkdown();
+      event.target.submit();
+    });
+  }
+
+  if (document.querySelector('#editNews')) {
+    editor.setMarkdown(document.querySelector('#oldText').value);
+
+    document.querySelector('#editNews').addEventListener('submit', event => {
+      event.preventDefault();
+      document.querySelector('#text').value = editor.getMarkdown();
+      event.target.submit();
+    });
+  }
 }
 
-if (document.querySelector('#editNews')) {
-  editor.setMarkdown(document.querySelector('#oldText').value)
+window.Alpine = Alpine;
 
-  document.querySelector('#editNews').addEventListener('submit', event => {
-    event.preventDefault()
-    document.querySelector('#text').value = editor.getMarkdown()
-    event.target.submit()
-  })
-}
-
-window.Alpine = Alpine
-
-Alpine.start()
+Alpine.start();
