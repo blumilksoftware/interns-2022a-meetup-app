a8112081314af2dcca9093517f71afe56c25cd9d
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-05-11 00:34:57 +0200

    #109 -  add news page

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -1,31 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container sm:w-[1216px] mx-auto px-5 xl:px-0">
-        <h1>News</h1>
-        <div>
-            <a href="{{ route('news.create') }}">Create new news</a>
-            @forelse ($news as $singleNews)
-                <div>
-                    {{ $singleNews->title }}
-                    {!! Str::markdown($singleNews->text) !!}
-                    {{ $singleNews->author }}
-                    <div>
-                        <a href="{{ route('news.edit', $singleNews) }}">Edit</a>
-                        <form action="{{ route('news.destroy', $singleNews) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit"
-                                    onclick="return confirm('Delete News? This operation is irreversible.')">
-                                Delete
-                            </button>
-                        </form>
-                    </div>
+  <div class="flex justify-end relative">
+    @include('partials.admin-navbar')
+    <div class="bg-white w-[1432px] rounded-20 m-12 px-10 py-6">
+      <div class="flex justify-between">
+        <h3 class="text-2xl font-semibold">News</h3>
+        <a href="{{ route('news.create') }}"
+          class="bg-green-600 text-white px-5 py-2 rounded-lg hover:bg-green-700">
+          Add news
+        </a>
+      </div>
+      <div class="relative text-gray-600 mt-5">
+        <input class="border-2 border-gray-300 bg-white h-10 px-10 pr-16 rounded-lg text-sm focus:outline-none w-80"
+          type="search" name="search" placeholder="Search" />
+        <button type="submit" class="absolute left-3 -top-3 mt-5 mr-4">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+      <table class="border border-collapse w-full mt-5">
+        <thead class="text-left">
+          <tr>
+            <th class="border pl-3 py-1">Id</th>
+            <th class="border pl-3 py-1">Title</th>
+            <th class="border pl-3 py-1">Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($news as $singleNews)
+            <tr class="odd:bg-gray-100">
+              <td class="border pl-3 py-1">{{ $singleNews->id }}</td>
+              <td class="border pl-3 py-1">{{ $singleNews->title }}</td>
+              <td class="border pl-3 py-1">{{ $singleNews->author }}</td>
+              <td class="border pl-3 py-1 w-52">
+                <div class="flex gap-3 text-white">
+                  <a href="{{ route('news.edit', $singleNews) }}"
+                    class="bg-indigo-600 hover:bg-indigo-700 text-sm px-2 py-0.5 rounded">
+                    <i class="fa-solid fa-pen-to-square mr-2"></i>edit
+                  </a>
+                  <form action="{{ route('news.destroy', $singleNews) }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button class="bg-red-500 hover:bg-red-600 text-sm px-2 py-0.5 rounded text-white">
+                      <i class="fa-solid fa-trash-can mr-2"></i>delete
+                    </button>
+                  </form>
                 </div>
-            @empty
-                <p>There are no news</p>
-            @endforelse
-        </div>
-        {{ $news->links('vendor.pagination.tailwind') }}
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
     </div>
+  </div>
 @endsection
