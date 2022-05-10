81659aa4780a1cf3d5e11e6a6f27b8e39fdc2378
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-05-10 22:32:17 +0200

    #109 - add admin navbar

---
+++ resources/views/partials/admin-navbar.blade.php
@@ -0,0 +1,46 @@
+<div class="h-screen bg-indigo-800 w-80 p-8 left-0 fixed">
+ <ul class="text-white">
+   <li class="hover:bg-indigo-600 px-4 py-2 rounded-lg mt-3 hover:cursor-pointer">
+     <a href="#" class="text-lg">
+       <i class="fa-solid fa-table-columns fa-lg mr-6"></i>
+       Dashboard
+     </a>
+   </li>
+   <li class="hover:bg-indigo-600 px-4 py-2 rounded-lg mt-3 hover:cursor-pointer">
+     <a href="#" class="text-lg">
+       <i class="fa-solid fa-circle-question fa-lg mr-6"></i>
+       Logo
+     </a>
+   </li>
+   <li class="hover:bg-indigo-600 px-4 py-2 rounded-lg mt-3 hover:cursor-pointer">
+     <a href="#" class="text-lg">
+       <i class="fa-solid fa-users fa-lg mr-5"></i>
+       Users
+     </a>
+   </li>
+   <li class="hover:bg-indigo-600 px-4 py-2 rounded-lg mt-3 hover:cursor-pointer">
+     <a href="#" class="text-lg">
+       <i class="fa-solid fa-calendar-check fa-lg mr-6"></i>
+       Meetups
+     </a>
+   </li>
+   <li class="hover:bg-indigo-600 px-4 py-2 rounded-lg mt-3 hover:cursor-pointer">
+     <a href="#" class="text-lg">
+       <i class="fa-solid fa-sitemap fa-lg mr-5"></i>
+       Organizations
+     </a>
+   </li>
+   <li class="hover:bg-indigo-600 px-4 py-2 rounded-lg mt-3 hover:cursor-pointer">
+     <a href="#" class="text-lg">
+       <i class="fa-solid fa-microphone fa-lg mr-6"></i>
+       Speakers
+     </a>
+   </li>
+   <li class="hover:bg-indigo-600 px-4 py-2 rounded-lg mt-3 hover:cursor-pointer">
+     <a href="#" class="text-lg">
+       <i class="fa-solid fa-newspaper fa-lg mr-6"></i>
+       News
+     </a>
+   </li>
+ </ul>
+</div>
