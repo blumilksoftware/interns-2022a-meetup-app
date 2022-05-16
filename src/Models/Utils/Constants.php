789d87963bf283865f3c59b84140050bc98ef347
Michał Gałęzyka <72552648+Majkulnn@users.noreply.github.com>
2022-05-16 15:03:28 +0200

    #88 - User profile system (#100)
    * #93 - fix routing and seeder
    
    * #93 - add password validation
    
    * #93 - change exception
    
    * #88 - create migration, controller and routing
    
    * #88 - create views
    
    * #88 - add user avatar in old files
    
    * #93 - Apply suggestions from code review
    
    Co-authored-by: Krzysztof Rewak <krzysztof.rewak@gmail.com>
    
    * #93 - change password hashing
    
    * #88 - add views, service, exceptions
    
    * #88 - cs-fix
    
    * #93 - changes from many to one view and controller
    
    * #88 - refactor file names
    
    * #88 - delete identifier.sqlite
    
    * #88 - change routes in view
    
    * #88 - fix after merge
    
    * #88 - change facades
    
    * #88 - cs:fix
    
    * #88 - fix avatar
    
    * #88 - Apply suggestions from code review
    
    Co-authored-by: MichalMyskow <72620908+MichalMyskow@users.noreply.github.com>
    
    * #88 - cs-fix
    
    * #88 - fix routing
    
    * #88 - fix
    
    * #88 - change white spaces
    
    Co-authored-by: Krzysztof Rewak <krzysztof.rewak@gmail.com>
    Co-authored-by: MichalMyskow <72620908+MichalMyskow@users.noreply.github.com>

--- src/Models/Utils/Constants.php
+++ src/Models/Utils/Constants.php
@@ -10,10 +10,12 @@ class Constants
     public const MEETUPS_LOGOS_PATH = "meetups/logos";
     public const ORGANIZATIONS_LOGOS_PATH = "organizations/logos";
     public const SPEAKERS_AVATARS_PATH = "speakers/avatars";
+    public const USERS_AVATARS_PATH = "users/avatars";
     public const NEWS_LOGOS_PATH = "news/logos";
     public const MEETUP_DEFAULT_LOGO_PATH = "static/images/meetup_card.webp";
     public const ORGANIZATION_DEFAULT_LOGO_PATH = "static/images/organization_logo.webp";
     public const SPEAKER_DEFAULT_AVATAR_PATH = "static/images/random_avatar.webp";
+    public const USER_DEFAULT_AVATAR_PATH = "static/images/random_avatar.webp";
     public const NEWS_DEFAULT_LOGO_PATH = "static/images/news_list.webp";
     public const STORAGE_PATH = "storage/";
     public const DEFAULT_PAGINATION = 20;
