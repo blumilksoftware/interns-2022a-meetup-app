dba88adc7d15a5b6f85699bb0fcbb33b470a3c7d
Michał Gałęzyka <majkulnspam@gmail.com>
2022-07-08 11:07:54 +0200

    #90 - add requests

---
+++ src/Http/Requests/Account/Login2FaUserCodeRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\Meetup\Core\Http\Requests\Account;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Login2FaUserCodeRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            "email" => ["required", "string", "email", "max:255"],
+            "code" => ["required", "string", "numeric", "max:6"],
+        ];
+    }
+}
---
+++ src/Http/Requests/Account/User2FaCodeRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\Meetup\Core\Http\Requests\Account;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class User2FaCodeRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            "code" => ["required", "string", "numeric", "max:6"],
+        ];
+    }
+}
