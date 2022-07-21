47e548b4e37d2071d9913a646b9df14e20e684dc
MichalMyskow <myskow.michal@gmail.com>
2022-07-21 02:30:15 +0200

    #124 - create enum for genders, update request and controller

--- src/Http/Requests/Account/UpdateUserDataRequest.php
+++ src/Http/Requests/Account/UpdateUserDataRequest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Blumilk\Meetup\Core\Http\Requests\Account;
 
+use Blumilk\Meetup\Core\Enums\AvailableGenders;
 use Blumilk\Meetup\Core\Http\Requests\Speaker\Rules\AvatarFileRules;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
 
 class UpdateUserDataRequest extends FormRequest
 {
@@ -17,6 +19,9 @@ class UpdateUserDataRequest extends FormRequest
             "email" => ["required", "string", "email", "max:255", "unique:users,email,{$userId},id"],
             "name" => ["required", "string", "max:255"],
             "avatar" => AvatarFileRules::rules(),
+            "location" => ["nullable", "string", "max:255"],
+            "birthday" => ["nullable", "date"],
+            "gender" => ["nullable", new Enum(AvailableGenders::class)],
         ];
     }
 }
