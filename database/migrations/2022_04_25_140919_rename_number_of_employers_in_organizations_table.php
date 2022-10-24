cbdc617eef0ff99f44c2174f393b01733cb2bdc0
Aleksander Kowalski <mrhdolek14@gmail.com>
2022-10-24 10:53:24 +0200

    fix: env docker compose

--- database/migrations/2022_04_25_140919_rename_number_of_employers_in_organizations_table.php
+++ database/migrations/2022_04_25_140919_rename_number_of_employers_in_organizations_table.php
@@ -8,10 +8,20 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
+    private const CONFIG_KEY_FOR_DEFAULT_DB = "database.default";
+
     public function up(): void
     {
-        DB::statement('ALTER TABLE organizations ALTER COLUMN 
-                  number_of_employers TYPE integer USING (number_of_employers)::integer');
+        if (config(self::CONFIG_KEY_FOR_DEFAULT_DB) === "pgsql") {
+            DB::statement("ALTER TABLE organizations ALTER COLUMN 
+                  number_of_employers TYPE integer USING (number_of_employers)::integer");
+        }
+
+        if (config(self::CONFIG_KEY_FOR_DEFAULT_DB) === "sqlite") {
+            Schema::table("organizations", function (Blueprint $table): void {
+                $table->integer("number_of_employers")->change();
+            });
+        }
 
         Schema::table("organizations", function (Blueprint $table): void {
             $table->renameColumn("number_of_employers", "number_of_employees");
